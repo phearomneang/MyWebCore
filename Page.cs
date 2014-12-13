1898b0d849e9a953b3f3a9016b98b7b47451b887
phearomneang <phearom.neang@outlook.com>
2014-12-13 11:14:03 +0700

    MyPage
    It is the base page for all page inherit from it.
    It has some properties and method that already define for the page.

---
+++ Page.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Page:System.Web.UI.Page
+{
+	public Page{}
+	public static int CurrentID
+	{
+		get
+		{
+			return Convert.ToInt32(HttpContext.Current.Request.QueryString["id"]);
+		}
+	}
+}
